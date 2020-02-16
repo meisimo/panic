1f38ef97bf65cd3dc5690d019f6cbfdc7bf14685
maximo silva <siruasor@gmail.com>
2020-02-16 00:26:37 -0500

    Corrections in the home view and add alert to the button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private infoIcon = [
+    {
+      'on_icon': 'volume-high',
+      'off_icon': 'volume-off',
+      'estado': true,
+      'indice': 'sonido',
+    },
+    {
+      'on_icon': 'notifications',
+      'off_icon': 'notifications-off',
+      'estado': true,
+      'indice': 'notificacion',
+    },
+    {
+      'on_icon': 'eye',
+      'off_icon': 'eye-off',
+      'estado': true,
+      'indice': 'visibilidad',
+    }
+  ]
+  constructor(
+    public alertController: AlertController
+  ){}
 
-  constructor() {}
+  ngOnInit(){
+    this.infoIcon.forEach( icon =>
+      icon['icon'] = icon.estado ? icon.on_icon : icon.off_icon 
+    )
+  }
 
+  async muestraMensageMandado(){
+    const alert = await this.alertController.create({
+      header: 'Señal enviada',
+      message: 'Señal de ayuda enviada!',
+      buttons: ['Aceptar']
+    })
+    await alert.present()
+  }
 }
