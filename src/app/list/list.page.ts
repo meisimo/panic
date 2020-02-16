189acc59e597080025bde416783122a0fb3aef5b
maximo silva <siruasor@gmail.com>
2020-02-16 00:07:19 -0500

    Corrections in the setting page

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -7,58 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
   private icons = [
-    // 'call',
-    // 'volume-high',
-    // 'contrast'
     {
       title: 'Emergencia',
-      note: '',
-      icon: 'call'
+      icon: 'call',
+      estado: false,
     },
     {
       title: 'Sonido',
-      note: 'barra',
-      icon: 'volume-high'
+      icon: 'volume-high',
+      estado: true,
     },
     {
-      
+      title: 'Alerta',
+      icon: 'notifications',
+      estado: true,
+    },
+    {      
       title: 'Modo oscuro ',
-      note: 'barra',
-      icon: 'contrast'
+      icon: 'contrast',
+      estado: false,
     }
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
+  ]
+  public items: Array<{ title: string; note: string; icon: string }> = []
   constructor() {
-    for (let i = 0; i < 3; i++) {
-      // if (i==0) {
-      //   this.items.push({
-      //     title: 'Emergencia',
-      //     note: 'This is item #' + i,
-      //     icon: this.icons[i]
-      //   });
-      // }else if(i==1){
-      //   this.items.push({
-      //     title: 'Sonido ' + i,
-      //     note: 'This is item #' + i,
-      //     icon: this.icons[i]
-      //   });
-      // }else{
-      //   this.items.push({
-      //     title: 'Modo oscuro ' + i,
-      //     note: 'This is item #' + i,
-      //     icon: this.icons[i]
-      //   });
-      // }
-      
-    }
   }
 
   ngOnInit() {
   }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
+
+  optionChange(){
+    console.log(this.icons.map(icon => icon.estado))
+  }
 }
