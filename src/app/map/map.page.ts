b95cc092d62fdd46184120ad7d359a5f6338ab7d
checholini <sejrl3@gmail.com>
2020-02-16 20:45:27 -0500

    added map render to map view

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
 
 @Component({
   selector: 'app-map',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('map') mapElement: ElementRef;
+
+  map: any;
+  mapInitialised: boolean = false;
+  apiKey: any;
+
+
+  constructor(public navCtrl: NavController) {
+    this.loadGoogleMaps();
+  }
 
   ngOnInit() {
   }
 
+  loadGoogleMaps() {
+    if (typeof google == "undefined" || typeof google.maps == "undefined") {
+
+      window['mapInit'] = () => {
+        this.initMap();
+        console.log("LOADING...");
+        
+      }
+
+      let script = document.createElement("script");
+      script.id = "googleMaps";
+
+      if (this.apiKey) {
+        script.src = 'http://maps.google.com/maps/api/js?key=' + this.apiKey + '&callback=mapInit';
+      } else {
+        script.src = 'http://maps.google.com/maps/api/js?callback=mapInit';
+      }
+
+      document.body.appendChild(script);
+
+
+    }
+    else {
+      console.log("showing map");
+      this.initMap();
+    }
+
+  }
+
+  initMap() {
+
+    this.mapInitialised = true;
+
+    Geolocation.getCurrentPosition().then((position) => {
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    });
+
+  }
 }
